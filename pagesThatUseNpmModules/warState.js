267626c0b64e23e466ca6ea969de9f6d41744bcb
jtank4 <pubgithub@yahoo.com>
2023-09-18 01:31:13 -0400

    New page to get warframe world state
    Not working yet, testing if CORS issue also happens on github pages

---
+++ pagesThatUseNpmModules/warState.js
@@ -0,0 +1,18 @@
+const WorldState = require("warframe-worldstate-parser");
+const webReq = new XMLHttpRequest();
+var pageState = {"ready":false};
+var warState;
+
+function readyPage(e){
+	console.log(webReq)
+	if(webReq.status == 200){
+		warState= new WorldState(webReq.responseText);
+		console.log(ws.alerts[0].toString());
+		pageState.ready = true;
+	}
+}
+
+webReq.onreadystatechange = readyPage;
+const warframeStateUrl = "http://content.warframe.com/dynamic/worldState.php";
+webReq.open("GET", warframeStateUrl);
+fetch("http://content.warframe.com/dynamic/worldState.php").then(data => console.log(data));
